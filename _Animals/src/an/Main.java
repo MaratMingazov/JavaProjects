4a0a3b18658d10320e0d917ac7c6491587837736
m.mingazov <m.mingazov@mmingazov.uni.innopolis.ru>
2015-12-01 10:29:24 +0300

    m.mingazov (new Cat("bay3"))

--- _Animals/src/an/Main.java
+++ _Animals/src/an/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {	
-		Animal cat1 = new Cat("bay1");
+		Animal cat1 = new Cat("bay3");
 		Animal cat2 = new Cat("bay2");
 		Animal dog1 = new Dog("Rocky");	
 		System.out.println("finish!!");
