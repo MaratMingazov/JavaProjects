44ff872b2c5c6c88d4a57eccb648d0ae78aceb5d
12z <12zrus@gmail.com>
2015-12-16 13:56:27 +0300

    Generated getters, setters, and constructors

--- Plugin_MemoryVizualization/src/plugin2/views/ActivationRecord.java
+++ Plugin_MemoryVizualization/src/plugin2/views/ActivationRecord.java
@@ -6,4 +6,48 @@ public class ActivationRecord {
 	private String startAddress;
 	private String endAddress;
 	private VarDescription[] vars;
+	
+	public ActivationRecord(String functionName, String fileName, String startAddress, String endAddress,
+			VarDescription[] vars) {
+		super();
+		this.functionName = functionName;
+		this.fileName = fileName;
+		this.startAddress = startAddress;
+		this.endAddress = endAddress;
+		this.vars = vars;
+	}
+	
+	// getters and setters
+	public String getFunctionName() {
+		return functionName;
+	}
+	public void setFunctionName(String functionName) {
+		this.functionName = functionName;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getStartAddress() {
+		return startAddress;
+	}
+	public void setStartAddress(String startAddress) {
+		this.startAddress = startAddress;
+	}
+	public String getEndAddress() {
+		return endAddress;
+	}
+	public void setEndAddress(String endAddress) {
+		this.endAddress = endAddress;
+	}
+	public VarDescription[] getVars() {
+		return vars;
+	}
+	public void setVars(VarDescription[] vars) {
+		this.vars = vars;
+	}
+	
+	
 }
--- Plugin_MemoryVizualization/src/plugin2/views/VarDescription.java
+++ Plugin_MemoryVizualization/src/plugin2/views/VarDescription.java
@@ -5,4 +5,40 @@ public class VarDescription {
 	private String type;
 	private String value;
 	private String name;
+	
+	public VarDescription(String address, String type, String value, String name) {
+		super();
+		this.address = address;
+		this.type = type;
+		this.value = value;
+		this.name = name;
+	}
+	
+	// getters and setters
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
