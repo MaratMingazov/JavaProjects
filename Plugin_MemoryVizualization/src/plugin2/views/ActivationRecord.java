509a6f738ace85e8522b23056109d1880ac23d50
12z <12zrus@gmail.com>
2015-12-16 13:38:06 +0300

    data structures creation

---
+++ Plugin_MemoryVizualization/src/plugin2/views/ActivationRecord.java
@@ -0,0 +1,9 @@
+package plugin2.views;
+
+public class ActivationRecord {
+	private String functionName;
+	private String fileName;
+	private String startAddress;
+	private String endAddress;
+	private VarDescription[] vars;
+}
---
+++ Plugin_MemoryVizualization/src/plugin2/views/VarDescription.java
@@ -0,0 +1,8 @@
+package plugin2.views;
+
+public class VarDescription {
+	private String address;
+	private String type;
+	private String value;
+	private String name;
+}
